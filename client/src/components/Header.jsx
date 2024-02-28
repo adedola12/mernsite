1644d327b2b82471db5051af9ec70215dcb519e1
adedola12 <dolapo836@gmail.com>
2024-02-28 12:22:09 +0100

    updated header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <header className="bg-blue-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -32,8 +34,19 @@ export default function Header() {
               About
             </li>
           </Link>
-          <Link to="/sign-up">
-            <li className="sm:inline text-blue-700 hover:underline">Sign Up</li>
+
+          <Link to="/profile">
+            {currentUser ? (
+              <img
+                src={currentUser.avatar}
+                alt={currentUser.username.slice(-2)}
+                className="rounde-full h-7 w-7 object-cover"
+              />
+            ) : (
+              <li className="sm:inline text-blue-700 hover:underline">
+                Sign Up
+              </li>
+            )}
           </Link>
         </ul>
       </div>
