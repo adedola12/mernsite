59e34c21f1fbafcd315b8ecfbe775f6aa1220683
adedola12 <dolapo836@gmail.com>
2024-03-30 09:18:40 +0100

    Add show more functionality

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -14,6 +14,7 @@ export default function Search() {
   });
   const [loading, setLoading] = useState(false);
   const [listings, setListings] = useState([]);
+  const [showMore, setShowMore] = useState(false);
 
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
@@ -47,10 +48,14 @@ export default function Search() {
 
     const fetchListing = async () => {
       setLoading(true);
+      setShowMore(false);
 
       const searchQuery = urlParams.toString();
       const res = await fetch(`/api/listing/get?${searchQuery}`);
       const data = await res.json();
+      if (data.length > 9) {
+        setShowMore(true);
+      }
 
       setListings(data);
       setLoading(false);
@@ -108,6 +113,21 @@ export default function Search() {
 
     const searchQuery = urlParams.toString();
     navigate(`/search?${searchQuery}`);
+  };
+
+  const onShowMoreClick = async () => {
+    const numberOfListings = listings.length;
+    const startIndex = numberOfListings;
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set("startIndex", startIndex);
+    const searchQuery = urlParams.toString();
+
+    const res = await fetch(`/api/listing/get?${searchQuery}`);
+    const data = await res.json();
+    if (data.length < 9) {
+      setShowMore(false);
+    }
+    setListings([...listings, ...data]);
   };
 
   return (
@@ -233,7 +253,14 @@ export default function Search() {
               <ListingItem key={listing._id} listing={listing} />
             ))}
         </div>
-        <Link>Show More</Link>
+        {showMore && (
+          <button
+            onClick={onShowMoreClick()}
+            className="text-green-700 hover:underline p-7 w-full"
+          >
+            Show More
+          </button>
+        )}
       </div>
     </div>
   );
