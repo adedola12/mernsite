17343dc07a05926da6a226b60af92e2e07e67ce4
adedola12 <dolapo836@gmail.com>
2024-06-22 14:08:34 +0100

    Dolapo: OAUTH Fixed

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -6,7 +6,7 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "mernsite.firebaseapp.com",
+  authDomain: "adlmmarketplace.vercel.app",
   projectId: "mernsite",
   storageBucket: "mernsite.appspot.com",
   messagingSenderId: "470385041562",
