0f0111b5a8b7816bdc64e7ec4298566f0c0963f6
Olawumi Olusegun <olawumi.olusegun@gmail.com>
2024-06-23 22:20:48 +0100

    upate

--- client/config/index.js
+++ client/config/index.js
@@ -1,9 +1,11 @@
 const prod = {
-    baseUrl: "https://adlmmarketplace-git-main-adedola12s-projects.vercel.app",
+    baseUrl: "https://adlmmarketplace.onrender.com",
   };
+
   // http://localhost:3000
+  // "https://adlmmarketplace.onrender.com
   const dev = {
-    baseUrl: "http://localhost:3000",
+    baseUrl: "https://mernsite-k2ky.onrender.com",
   };
   
   export const config = process.env.NODE_ENV === "development" ? dev : prod;
