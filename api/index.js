04c9475a8306f4ffb89aaa2d613144249347dc6d
Olawumi Olusegun <olawumi.olusegun@gmail.com>
2024-06-23 23:23:25 +0100

    new commit

--- api/index.js
+++ api/index.js
@@ -11,14 +11,28 @@ import appConstants from "./constants/index.js";
 
 const app = express();
 
-app.use(cors({origin: [
+
+const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "http://localhost:3000",
   "https://adlmmarketplace-git-main-adedola12s-projects.vercel.app"
-],
-    credentials: true
-}))
+];
+
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+};
+
+
+app.use(cors(corsOptions))
 app.use(express.json());
 app.use(cookieParser());
 
