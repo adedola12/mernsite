584f7b24044d2404d10d52351660ff49426670ff
Olawumi Olusegun <olawumi.olusegun@gmail.com>
2024-07-02 13:15:05 +0100

    updt16

--- client/src/components/MarketPlace.jsx
+++ client/src/components/MarketPlace.jsx
@@ -94,9 +94,11 @@ export default function Marketplace() {
     if (!hasMore) return;
 
     try {
+
       setIsLoadingProduct(true);
 
       const fetchUrl = `${config.baseUrl}/api/product/getCat?page=${page}&limit=${MAX_LIMIT}`;
+
       const res = await fetch(fetchUrl, { credentials: "include" });
 
       if (!res.ok) {
@@ -155,9 +157,9 @@ export default function Marketplace() {
           <div className="flex flex-col gap-6 bg-white p-5 rounded-md ">
             <h2 className="font-semibold text-3xl ">ADLM Marketplace</h2>
             <div className="flex flex-col md:flex-row gap-4 w-full">
-              <div className="relative border bg-white rounded-lg flex gap-6 items-center md:w-[205px] justify-between">
+              <div className="relative border bg-white rounded-lg flex gap-6 items-center  md:w-[205px] justify-between">
                 
-                <input name="name" onChange={handleLocationInput} type="text" placeholder="Location" className="px-2 text-[#818181] font-semibold size-full pr-7 "/>
+                <input name="name" onChange={handleLocationInput} type="text" placeholder="Location" className="px-2 py-5  text-[#818181] font-semibold size-full pr-7 "/>
               
                 <MdLocationOn className="text-[#CFCFCF] h-[16px] w-[16px] absolute top-[50%] -translate-y-[50%] right-2" />
               </div>
