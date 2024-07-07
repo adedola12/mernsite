7fcfd386ed51bef3ff4602c14eec7c5ac0f49224
Olawumi Olusegun <olawumi.olusegun@gmail.com>
2024-07-07 22:14:06 +0100

    updt35

--- client/src/components/PageLoader.jsx
+++ client/src/components/PageLoader.jsx
@@ -6,7 +6,7 @@ import { ImSpinner9 } from "react-icons/im";
 const PageLoader = () => {
 
   return (
-    <div  className="fixed z-50 inset-0 bg-white bg-opacity-5 backdrop-blur-md overflow-y-auto min-h-screen w-full" >
+    <div  className="fixed z-50 inset-0 bg-white bg-opacity-5 backdrop-blur-md overflow-y-auto min-h-dvh w-full" >
     <div className="w-full h-full flex flex-col items-center justify-center">
         <ImSpinner9 className='text-[#00263D] animate-spin' size={50} />
     </div>
