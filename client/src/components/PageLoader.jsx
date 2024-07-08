9181c067db438d0da19f473566a2a698f4053355
Olawumi Olusegun <olawumi.olusegun@gmail.com>
2024-07-08 07:34:05 +0100

    updt38

--- client/src/components/PageLoader.jsx
+++ client/src/components/PageLoader.jsx
@@ -7,7 +7,7 @@ const PageLoader = () => {
 
   return (
     <div  className="fixed z-50 inset-0 bg-white bg-opacity-5 backdrop-blur-md overflow-y-auto min-h-dvh w-full" >
-    <div className="w-full h-screen flex flex-col items-center justify-center">
+    <div className="w-full h-dvh flex flex-col items-center justify-center">
         <ImSpinner9 className='text-[#00263D] animate-spin' size={50} />
     </div>
   </div>
